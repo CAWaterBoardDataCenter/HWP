44ae248e2ee9bc39b8c9c8c77f9707f97ef36e2a
Corey Clatterbuck <corey.clatterbuck@gmail.com>
2022-05-03 18:51:51 -0700

    added artificial drainage & markdown headers

--- WatershedCondition.R
+++ WatershedCondition.R
@@ -1,13 +1,11 @@
-#################################
-#################################
-## Reading in Watershed Condition files for HWP re-assessment
+
+# Watershed Condition.R ----
 ## Started: 17 Aug 2021
 ## Edited: 2 May 2022
-#################################
-#################################
 
 
-## libraries
+
+##  read in libraries ----
 library(readr)
 library(tidyverse)
 library(nhdplusTools) ## requires download first using devtools::install_github("usgs-r/nhdplusTools")
@@ -15,20 +13,31 @@ library(here)
 library(scales)
 
 
-## read in data files
+## read in data files ----
 catch_ca <- readRDS(here("UpdatedData", "nhdplus_catchment_ca_cropped.rds"))
 NLCDcatch <- read_csv("./UpdatedData/NLCD2016_CA.csv") %>% ## Streamcat NLCD data
   select(COMID, CatAreaSqKm, PctOw2016Cat, PctIce2016Cat, PctBl2016Cat, PctDecid2016Cat, PctConif2016Cat, PctMxFst2016Cat, PctShrb2016Cat, PctGrs2016Cat, PctWdWet2016Cat, PctHbWet2016Cat, PctUrbOp2016Cat, PctUrbLo2016Cat, PctUrbMd2016Cat, PctUrbHi2016Cat, PctHay2016Cat, PctCrop2016Cat)
 COMIDs <- as.vector(NLCDcatch$COMID)
-Soil <- read.table(here("UpdatedData" , "STATSGO_LAYER_CAT_CONUS.txt"), header = TRUE, sep = ",") %>% 
+Soil <- read.table(here("UpdatedData" , "STATSGO_LAYER_CAT_CONUS.txt"), 
+                   header = TRUE, sep = ",") %>% 
   select(COMID, CAT_RFACT, CAT_KFACT, NODATA) %>%
   dplyr::filter(COMID %in% COMIDs) ## NHDPlusV2 STATSGO data
-Slope <- read.table(here("UpdatedData" , "BASIN_CHAR_CAT_CONUS.txt"), header = TRUE, sep = ",") %>% 
+Slope <- read.table(here("UpdatedData" , "BASIN_CHAR_CAT_CONUS.txt"), 
+                    header = TRUE, sep = ",") %>% 
   dplyr::filter(COMID %in% COMIDs) ## NHDPlusV2 basin slope
+AgDitches92 <- read.table(here("UpdatedData" , "DITCHES92_CONUS.txt"), 
+                          header = TRUE, sep = ",") %>%
+  dplyr::filter(COMID %in% COMIDs)
+AgTiles92 <- read.table(here("UpdatedData" , "TILES92_CONUS.txt"), 
+                        header = TRUE, sep = ",") %>%
+  dplyr::filter(COMID %in% COMIDs)
+AgTilesNakagaki <- read.table(here("UpdatedData" , "TILES_Nakagaki_CONUS.txt"), 
+                              header = TRUE, sep = ",") %>%
+  dplyr::filter(COMID %in% COMIDs)
 CatRdx <- read_csv("./UpdatedData/RoadStreamCrossings_CA.csv") %>% ## Streamcat Road Crossings data
   dplyr::select(COMID, RdCrsCat)
 
-## read in functions
+## read in functions ----
 source(here("functions", "normalrank.R"))
 
 ## test whether values are similar between Streamcat and NLCD datasets (some are not)
@@ -52,8 +61,9 @@ source(here("functions", "normalrank.R"))
 colnames(NLCDcatch)
 str(NLCDcatch)
 
+## indicator calculations ----
 
-### Percent natural land cover
+### Percent natural land cover ----
 NLCD.df <- NLCDcatch[,c(1, 3:12)]
 NLCD.df[!complete.cases(NLCD.df), ] ## has 1 COMID without any land cover data
 NLCD.df$PctNatCover <- round(rowSums(NLCD.df[2:11]),2)
@@ -61,10 +71,10 @@ NLCD.df$nrank_PctNatCover <- normalrank(NLCD.df$PctNatCover)
 NLCD.df <- select(NLCD.df, COMID, nrank_PctNatCover)
 NLCD.df[!complete.cases(NLCD.df), ] ## same COMID as above was not ranked
 
-### ARA -- need ArcGIS to complete
+### ARA: need ArcGIS to complete ----
 
 
-### Sedimentation Risk
+### Sedimentation Risk ----
 
 ## looking for R factor: looks like was pre-calculated in the obs variables from 2013.
 # data2013 <- read_csv("./CA_HWI_FinalData_101813/Deliverables/CAdata_obs.csv")
@@ -123,32 +133,60 @@ sedrisk.df$nrank_sedrisk <- normalrank(-sedrisk.df$sedrisk)
 Sedrisk.df <- select(sedrisk.df, COMID, sedrisk, nrank_sedrisk)
 
 
-### Percent artificial drainage area
+### Percent artificial drainage area ----
+#### compare AgTiles92 to AgTilesNakagaki
+filterT1 <- AgTiles92 %>%
+  dplyr::filter(CAT_TILES92 > 0) %>%
+  dplyr::select(COMID, CAT_TILES92, CAT_NODATA) ## why does this dataset have more obs?
+filterT2 <- AgTilesNakagaki %>%
+  dplyr::filter(CAT_TILES_Early90s > 0) %>%
+  dplyr::select(COMID, CAT_TILES_Early90s, CAT_NODATA)
+
+#### get tiles and ditches in same dataset & sum percentages
+AgCombo <- AgDitches92 %>%
+  dplyr::select(COMID, CAT_DITCHES92) %>%
+  left_join(AgTiles92, by = "COMID") %>%
+  dplyr::select(COMID, CAT_DITCHES92, CAT_TILES92) %>%
+  left_join(AgTilesNakagaki, by = "COMID") %>%
+  dplyr::select(COMID, CAT_DITCHES92, CAT_TILES92, CAT_TILES_Early90s) %>%
+  dplyr::mutate(across(CAT_DITCHES92:CAT_TILES_Early90s, ~na_if(.,-9999))) %>% ## make no data to na
+  rowwise() %>%
+  mutate(Drainage92 = round(sum(CAT_DITCHES92,CAT_TILES92,na.rm=TRUE),2),
+         Drainage90s = round(sum(CAT_DITCHES92,CAT_TILES_Early90s,na.rm=TRUE),2))
+
+#### rank-normalize & create .df
+AgCombo$nrank_Drainage92 <- normalrank(-AgCombo$Drainage92)
+AgCombo$nrank_Drainage90s <- normalrank(-AgCombo$Drainage90s)
+Ag.df <- dplyr::select(AgCombo, COMID, nrank_Drainage92, nrank_Drainage90s)
 
 
-### Dam storage ratio
+### Dam storage ratio ----
 
 
-### Road crossing density: RdCrsCat in CatRdx
+### Road crossing density: RdCrsCat in CatRdx ----
 Rdx.df <- CatRdx
 Rdx.df$nrank_RdCrsCat <- normalrank(-Rdx.df$RdCrsCat)
 Rdx.df <- select(Rdx.df, COMID, RdCrsCat, nrank_RdCrsCat)
 
 
 
-## join dfs together, add to catch_ca
+## join dfs together, add to catch_ca ----
 catch_ca_plot <- catch_ca %>%
   left_join(NLCD.df, by = c("FEATUREID" = "COMID")) %>%
   left_join(Rdx.df, by = c("FEATUREID" = "COMID")) %>%
-  left_join(Sedrisk.df, by = c("FEATUREID" = "COMID"))
+  left_join(Sedrisk.df, by = c("FEATUREID" = "COMID")) %>%
+  left_join(Ag.df, by = c("FEATUREID" = "COMID"))
 
 
-## Try some mapping
+## Try some mapping ----
 library(sf)
 library(viridisLite)
 str(catch_ca)
 
-### map whole state
+### set figure parameters ----
+breaks <- seq(0,1,.1)
+
+### map whole state ----
 # png(here("figures", "PctNatCover.png"), width = 6, height = 5, units = "in", res = 300)
 ggplot() +
   geom_sf(data = catch_ca_plot, mapping = aes(fill = nrank_PctNatCover), colour = NA) +
@@ -167,6 +205,28 @@ ggplot() +
   ggtitle("Sedimentation risk, rank-normalized")
 dev.off()
 
+# png(here("figures", "ArtificialDrainage92.png"), width = 6, height = 5, units = "in", res = 300)
+ggplot() +
+  geom_sf(data = catch_ca_plot, mapping = aes(fill = nrank_Drainage92), colour = NA) +
+  scale_fill_viridis_c(breaks = c(0.01, 1), labels = c("more area", "less area"))+
+  labs(fill = NULL) +
+  theme_bw() +
+  theme(legend.position = c(0.25, 0.08),
+        legend.direction = "horizontal") +
+  ggtitle("% artificial drainage area (1992), rank-normalized")
+dev.off()
+
+# png(here("figures", "ArtificialDrainage90s.png"), width = 6, height = 5, units = "in", res = 300)
+ggplot() +
+  geom_sf(data = catch_ca_plot, mapping = aes(fill = nrank_Drainage90s), colour = NA) +
+  scale_fill_viridis_c(breaks = c(0.01, 1), labels = c("more area", "less area"))+
+  labs(fill = NULL) +
+  theme_bw() +
+  theme(legend.position = c(0.25, 0.08),
+        legend.direction = "horizontal") +
+  ggtitle("% artificial drainage area (1990s), rank-normalized")
+dev.off()
+
 
 # png(here("figures", "Rdx.png"), width = 6, height = 5, units = "in", res = 300)
 ggplot() +
@@ -178,7 +238,7 @@ ggplot() +
 dev.off()
 
 
-## try a subset to map
+### try a subset to map ----
 catch_ca[which.max(catch_ca$s_RdCrsCat),]
 ggplot() + geom_sf(data = catch_ca, mapping = aes(fill = nrank_RdCrsCat), colour = NA) + 
   coord_sf(xlim = c(-122.2, -122.4),
